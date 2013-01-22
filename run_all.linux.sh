4d9cf406cd9520944d1e9c74b60b9c5306a3e272
Vinicius Jarina <viniciusjarina@gmail.com>
2013-01-22 03:08:51 -0200

    Added Travis.CI hook.

---
+++ run_all.linux.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# If you have mono x86 installed on a amd64 linux.
+#export CFLAGS=-m32
+#export CXXFLAGS=-m32
+#export LDFLAGS=-m32
+make -f Makefile.Linux
+export LD_LIBRARY_PATH=$PWD/external/lua/linux/lib64
+#export LD_LIBRARY_PATH=$PWD/external/lua/linux/lib
+xbuild KeraLua.sln /p:Configuration=Release
+cd tests/
+nunit-console KeraLua.Tests.dll
