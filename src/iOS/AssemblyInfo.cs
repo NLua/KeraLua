154c8bd3bb3490ff004dbbfb05bf219e37bda54b
Vinicius Jarina <viniciusjarina@gmail.com>
2013-03-24 01:07:55 -0300

    Fixed Warning (iOS)

--- src/iOS/AssemblyInfo.cs
+++ src/iOS/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
+[assembly: CLSCompliantAttribute (true)]
 [assembly: LinkWith("libLua51iOS.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.ArmV7s, Frameworks = "Foundation", ForceLoad = true, IsCxx = true, LinkerFlags = "-lstdc++")]
