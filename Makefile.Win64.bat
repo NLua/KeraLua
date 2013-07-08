da02ed9233026812f4e21f0792bda6e1566966bd
Vinicius Jarina <viniciusjarina@gmail.com>
2013-07-08 15:58:37 -0300

    Fixed 64bit build

--- Makefile.Win64.bat
+++ Makefile.Win64.bat
@@ -3,5 +3,5 @@ mkdir win64
 cd win64
 cmake .. -G "Visual Studio 11 Win64"
 cmake --build . --config Release
-copy "bin\lua52.dll" "..\..\..\tests\lua52.dll"
+copy "bin64\lua52.dll" "..\..\..\tests\lua52.dll"
 cd ../../..
