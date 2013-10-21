cad5427f96a80044a353f1f480dd186a0ae9c9a3
Vinicius Jarina <viniciusjarina@gmail.com>
2013-10-20 21:24:39 -0400

    Bumped version.

--- src/iOS/AssemblyInfo.cs
+++ src/iOS/AssemblyInfo.cs
@@ -3,3 +3,11 @@ using MonoTouch.ObjCRuntime;
 
 [assembly: CLSCompliantAttribute (true)]
 [assembly: LinkWith("libLua52iOS.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.ArmV7s, Frameworks = "Foundation", ForceLoad = true, IsCxx = true, LinkerFlags = "-lstdc++")]
+[assembly: AssemblyTitle ("KeraLua")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("KeraLua")]
+[assembly: AssemblyCopyright ("Copyright ©  2013 Vinicius Jarina")]
+[assembly: AssemblyVersion ("1.2.0.0")]
+[assembly: AssemblyFileVersion ("1.2.0.0")]
