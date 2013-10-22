bfa01f2256284a2eb3604a6dcc96e0f3795847d7
Vinicius Jarina <viniciusjarina@gmail.com>
2013-10-22 08:12:51 -0400

    Fixed iOS build

--- src/iOS/AssemblyInfo.cs
+++ src/iOS/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using System.Reflection;
 
 [assembly: CLSCompliantAttribute (true)]
 [assembly: LinkWith("libLua52iOS.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.ArmV7s, Frameworks = "Foundation", ForceLoad = true, IsCxx = true, LinkerFlags = "-lstdc++")]
