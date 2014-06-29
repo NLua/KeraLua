92c46b95071f5c0aefd9033b25fd7d215cf8879a
Vinicius Jarina <viniciusjarina@gmail.com>
2014-06-29 18:47:35 -0300

    [build] Added Android script build - Changed Win64 build to VS2013

--- Makefile.Win64.bat
+++ Makefile.Win64.bat
@@ -1,7 +1,7 @@
 cd external/lua
 mkdir win64
 cd win64
-cmake .. -G "Visual Studio 11 Win64"
+cmake .. -G "Visual Studio 12 Win64"
 cmake --build . --config Release
 copy "bin64\lua52.dll" "..\..\..\tests\lua52.dll"
 cd ../../..
