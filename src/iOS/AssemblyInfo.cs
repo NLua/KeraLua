37c0b9fb6b0df3e9d651dd34463d82f6cfba6556
Vinicius Jarina <viniciusjarina@gmail.com>
2015-05-11 09:46:32 -0300

    Bumped lua (x86-64 fix)

--- src/iOS/AssemblyInfo.cs
+++ src/iOS/AssemblyInfo.cs
@@ -3,12 +3,12 @@ using ObjCRuntime;
 using System.Reflection;
 
 [assembly: CLSCompliantAttribute (true)]
-[assembly: LinkWith("libLua52iOS.a", LinkTarget.Simulator | LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s, Frameworks = "Foundation", ForceLoad = true, IsCxx = true, LinkerFlags = "-lstdc++")]
+[assembly: LinkWith("libLua52iOS.a", LinkTarget.Thumb |LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s, Frameworks = "Foundation", ForceLoad = true, IsCxx = true, LinkerFlags = "-lstdc++")]
 [assembly: AssemblyTitle ("KeraLua")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("KeraLua")]
-[assembly: AssemblyCopyright ("Copyright ©  2014 Vinicius Jarina")]
-[assembly: AssemblyVersion ("1.3.2.0")]
-[assembly: AssemblyFileVersion ("1.3.2.0")]
+[assembly: AssemblyCopyright ("Copyright ©  2015 Vinicius Jarina")]
+[assembly: AssemblyVersion ("1.3.2.1")]
+[assembly: AssemblyFileVersion ("1.3.2.1")]
