ab49f5e2597206e941ef1f1ce5c163a6cb319467
Vinicius Jarina <viniciusjarina@gmail.com>
2019-01-10 00:21:08 -0500

    Fix conflicts 27 (#28)
    * Bump NUnit test adapter.
    
    * * Fixed Lua path on target files.
    
    * * Fixed Android build on Windows
    * Fixed .NET core build.
    
    * * Add `Release/DebugCore` configuration
    
    * msbuild/mono/vs isn't very reliable mixing not core and core builds https://developercommunity.visualstudio.com/content/problem/10334/your-project-is-not-referencing-the-netframeworkve.html
    
    * * Add netstandard build.
    * moved tests project to avoid bin/obj conflict.
    
    * * Fixed KeraLuaTest build.
    
    * * Fixed build dependency

--- tests/Tests/Interop.cs
+++ tests/Tests/Interop.cs
@@ -10,56 +10,56 @@ using KeraLua;
 
 namespace KeraLuaTest.Tests
 {
-	[TestFixture]
-	public class Interop
-	{
-		public static readonly char UnicodeChar = '\uE007';
-		public static string UnicodeString => Convert.ToString (UnicodeChar);
+    [TestFixture]
+    public class Interop
+    {
+        public static readonly char UnicodeChar = '\uE007';
+        public static string UnicodeString => Convert.ToString(UnicodeChar);
 
 #if MONOTOUCH
-        [MonoPInvokeCallback (typeof (LuaFunction))]
+        [MonoPInvokeCallback(typeof(LuaFunction))]
 #endif
-		static int TestUnicodeString (IntPtr p)
-		{
-			var state = Lua.FromIntPtr (p);
-			string param = state.ToString (1, false);
+        static int TestUnicodeString(IntPtr p)
+        {
+            var state = Lua.FromIntPtr(p);
+            string param = state.ToString(1, false);
 
-			Assert.AreEqual (UnicodeString, param, "#1 ToString()");
+            Assert.AreEqual(UnicodeString, param, "#1 ToString()");
 
-			return 0;
-		}
+            return 0;
+        }
 
-		public static LuaFunction FuncTestUnicodeString = TestUnicodeString;
+        public static LuaFunction FuncTestUnicodeString = TestUnicodeString;
 
-		[Test]
-		public void TestUnicodeString ()
-		{
-			Lua state = new Lua ();
-			state.Encoding = Encoding.UTF8;
-			state.PushCFunction (FuncTestUnicodeString);
-			state.SetGlobal ("TestUnicodeString");
-			state.PushString (UnicodeString);
-			state.SetGlobal ("unicodeString");
-			AssertString ("TestUnicodeString(unicodeString)", state);
-		}
+        [Test]
+        public void TestUnicodeString()
+        {
+            Lua state = new Lua();
+            state.Encoding = Encoding.UTF8;
+            state.PushCFunction(FuncTestUnicodeString);
+            state.SetGlobal("TestUnicodeString");
+            state.PushString(UnicodeString);
+            state.SetGlobal("unicodeString");
+            AssertString("TestUnicodeString(unicodeString)", state);
+        }
 
-		void AssertString (string chunk, Lua state)
-		{
-			string error = string.Empty;
+        void AssertString(string chunk, Lua state)
+        {
+            string error = string.Empty;
 
-			LuaStatus result = state.LoadString (chunk);
+            LuaStatus result = state.LoadString(chunk);
 
-			if (result != LuaStatus.OK)
-				error = state.ToString (1, false);
+            if(result != LuaStatus.OK)
+                error = state.ToString(1, false);
 
-			Assert.True (result == LuaStatus.OK, "Fail loading string: " + chunk + "ERROR:" + error);
+            Assert.True(result == LuaStatus.OK, "Fail loading string: " + chunk + "ERROR:" + error);
 
-			result = state.PCall (0, -1, 0);
+            result = state.PCall(0, -1, 0);
 
-			if (result != 0)
-				error = state.ToString (1, false);
+            if(result != 0)
+                error = state.ToString(1, false);
 
-			Assert.True (result == 0, "Fail calling chunk: " + chunk + " ERROR: " + error);
-		}
-	}
+            Assert.True(result == 0, "Fail calling chunk: " + chunk + " ERROR: " + error);
+        }
+    }
 }
