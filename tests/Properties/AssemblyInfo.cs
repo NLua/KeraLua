a304b907e19755e168ba89cfbd2a33d92b64d13c
Vinicius Jarina <viniciusjarina@gmail.com>
2019-01-12 19:36:20 -0500

    * Fixing `runtimes` folder for NuGet package.

--- tests/Properties/AssemblyInfo.cs
+++ tests/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
 
-[assembly: AssemblyInformationalVersion("0.1.0+148.Branch.master.Sha.a5fff8818aa25d22612dc7c41a66dee32aa5031a")]
+[assembly: AssemblyInformationalVersion("0.1.0+149.Branch.master.Sha.1a69c1ae045933d62449d3a46e988253995810f9")]
