4d2b02c3ac15d2921d90a422d9b90c55e33495d4
Vlad Soroka <vladiso@microsoft.com>
2021-05-11 22:16:47 -0700

    Addressing comments

--- src/DelegateExtensions.cs
+++ src/DelegateExtensions.cs
@@ -10,22 +10,14 @@ namespace KeraLua
             if (ptr == IntPtr.Zero)
                 return null;
 
-#if NETFRAMEWORK
-            return (LuaFunction) Marshal.GetDelegateForFunctionPointer(ptr, typeof(LuaFunction));
-#else
             return Marshal.GetDelegateForFunctionPointer<LuaFunction>(ptr);
-#endif
         }
 
         public static IntPtr ToFunctionPointer(this LuaFunction d)
         {
             if (d == null)
                 return IntPtr.Zero;
-#if NETFRAMEWORK
-            return Marshal.GetFunctionPointerForDelegate(d);
-#else
             return Marshal.GetFunctionPointerForDelegate<LuaFunction>(d);
-#endif
         }
 
         public static LuaHookFunction ToLuaHookFunction (this IntPtr ptr)
@@ -33,11 +25,7 @@ namespace KeraLua
             if (ptr == IntPtr.Zero)
                 return null;
 
-#if NETFRAMEWORK
-            return (LuaHookFunction) Marshal.GetDelegateForFunctionPointer(ptr, typeof(LuaHookFunction));
-#else
             return Marshal.GetDelegateForFunctionPointer<LuaHookFunction>(ptr);
-#endif
         }
 
         public static IntPtr ToFunctionPointer (this LuaHookFunction d)
@@ -45,11 +33,7 @@ namespace KeraLua
             if (d == null)
                 return IntPtr.Zero;
 
-#if NETFRAMEWORK
-            return Marshal.GetFunctionPointerForDelegate(d);
-#else
             return Marshal.GetFunctionPointerForDelegate<LuaHookFunction>(d);
-#endif
         }
 
         public static LuaKFunction ToLuaKFunction (this IntPtr ptr)
@@ -57,11 +41,7 @@ namespace KeraLua
            if(ptr == IntPtr.Zero)
                 return null;
 
-#if NETFRAMEWORK
-            return (LuaKFunction) Marshal.GetDelegateForFunctionPointer(ptr, typeof(LuaKFunction));
-#else
             return Marshal.GetDelegateForFunctionPointer<LuaKFunction>(ptr);
-#endif
         }
 
         public static IntPtr ToFunctionPointer (this LuaKFunction d)
@@ -69,11 +49,7 @@ namespace KeraLua
             if (d == null)
                 return IntPtr.Zero;
 
-#if NETFRAMEWORK
-            return Marshal.GetFunctionPointerForDelegate(d);
-#else
             return Marshal.GetFunctionPointerForDelegate<LuaKFunction>(d);
-#endif
         }
 
         public static LuaReader ToLuaReader (this IntPtr ptr)
@@ -81,11 +57,7 @@ namespace KeraLua
             if(ptr == IntPtr.Zero)
                 return null;
 
-#if NETFRAMEWORK
-            return (LuaReader) Marshal.GetDelegateForFunctionPointer(ptr, typeof(LuaReader));
-#else
             return Marshal.GetDelegateForFunctionPointer<LuaReader>(ptr);
-#endif
         }
 
         public static IntPtr ToFunctionPointer (this LuaReader d)
@@ -93,11 +65,7 @@ namespace KeraLua
             if (d == null)
                 return IntPtr.Zero;
 
-#if NETFRAMEWORK
-            return Marshal.GetFunctionPointerForDelegate(d);
-#else
             return Marshal.GetFunctionPointerForDelegate<LuaReader>(d);
-#endif
         }
 
         public static LuaWriter ToLuaWriter (this IntPtr ptr)
@@ -105,11 +73,7 @@ namespace KeraLua
             if(ptr == IntPtr.Zero)
                 return null;
 
-#if NETFRAMEWORK
-            return (LuaWriter) Marshal.GetDelegateForFunctionPointer(ptr, typeof(LuaWriter));
-#else
             return Marshal.GetDelegateForFunctionPointer<LuaWriter>(ptr);
-#endif
         }
 
         public static IntPtr ToFunctionPointer (this LuaWriter d)
@@ -117,11 +81,7 @@ namespace KeraLua
             if (d == null)
                 return IntPtr.Zero;
 
-#if NETFRAMEWORK
-            return Marshal.GetFunctionPointerForDelegate(d);
-#else
             return Marshal.GetFunctionPointerForDelegate<LuaWriter>(d);
-#endif
         }
 
         public static LuaAlloc ToLuaAlloc (this IntPtr ptr)
@@ -129,11 +89,7 @@ namespace KeraLua
             if(ptr == IntPtr.Zero)
                 return null;
 
-#if NETFRAMEWORK
-            return (LuaAlloc) Marshal.GetDelegateForFunctionPointer(ptr, typeof(LuaAlloc));
-#else
             return Marshal.GetDelegateForFunctionPointer<LuaAlloc>(ptr);
-#endif
         }
 
         public static IntPtr ToFunctionPointer (this LuaAlloc d)
@@ -141,11 +97,7 @@ namespace KeraLua
             if (d == null)
                 return IntPtr.Zero;
 
-#if NETFRAMEWORK
-            return Marshal.GetFunctionPointerForDelegate(d);
-#else
             return Marshal.GetFunctionPointerForDelegate<LuaAlloc>(d);
-#endif
         }
 
         public static LuaWarnFunction ToLuaWarning(this IntPtr ptr)
@@ -153,11 +105,7 @@ namespace KeraLua
             if (ptr == IntPtr.Zero)
                 return null;
 
-#if NETFRAMEWORK
-            return (LuaWarnFunction)Marshal.GetDelegateForFunctionPointer(ptr, typeof(LuaWarnFunction));
-#else
             return Marshal.GetDelegateForFunctionPointer<LuaWarnFunction>(ptr);
-#endif
         }
 
         public static IntPtr ToFunctionPointer(this LuaWarnFunction d)
@@ -165,11 +113,7 @@ namespace KeraLua
             if (d == null)
                 return IntPtr.Zero;
 
-#if NETFRAMEWORK
-            return Marshal.GetFunctionPointerForDelegate(d);
-#else
             return Marshal.GetFunctionPointerForDelegate<LuaWarnFunction>(d);
-#endif
         }
     }
 }
