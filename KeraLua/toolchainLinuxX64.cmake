6a63aaa830da343eafc23c2aaf8ab3cbe049a2bb
theKBro <K.Broselge@gmx.net>
2021-08-27 17:07:21 +0200

    - only support net5.0 - first shot add native linux lib

---
+++ KeraLua/toolchainLinuxX64.cmake
@@ -0,0 +1,17 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Linux)
+
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_C_FLAGS -m64)
+set(CMAKE_CXX_COMPILER g++)
+set(CMAKE_CXX_FLAGS -m64)
+
+set(CMAKE_EXE_LINKER_FLAGS -m64)
+set(CMAKE_MODULE_LINKER_FLAGS -m64)
+set(CMAKE_SHARED_LINKER_FLAGS -m64)
+# set(CMAKE_STATIC_LINKER_FLAGS -m64)
+
+    
+    
+    
