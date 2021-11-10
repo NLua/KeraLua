baf9444674aadf1746bca05c9b59e2d1bfe25738
Vinicius Jarina <viniciusjarina@gmail.com>
2021-11-09 16:55:09 -0800

    Fix build

--- src/LuaRegistry.cs
+++ src/LuaRegistry.cs
@@ -3,7 +3,9 @@
     /// <summary>
     /// Enum for pseudo-index used by registry table
     /// </summary>
+#pragma warning disable CA1008 // Enums should have zero value
     public enum LuaRegistry
+#pragma warning restore CA1008 // Enums should have zero value
     {
         /* LUAI_MAXSTACK		1000000 */
         /// <summary>
@@ -15,7 +17,9 @@
     /// <summary>
     /// Registry index 
     /// </summary>
+#pragma warning disable CA1008 // Enums should have zero value
     public enum LuaRegistryIndex
+#pragma warning restore CA1008 // Enums should have zero value
     {
         /// <summary>
         ///  At this index the registry has the main thread of the state.
